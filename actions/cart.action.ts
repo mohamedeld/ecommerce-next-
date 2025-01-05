77ed9cb893be0087a388d14783f3c5faa6f8670b
mohamedeld <mohamed.azoz20010@gmail.com>
2025-01-05 22:17:17 +0200

    add to cart functionality

--- actions/cart.action.ts
+++ actions/cart.action.ts
@@ -1,9 +1,29 @@
 'use server';
 
 import { auth } from "@/auth";
+import { prisma } from "@/db/initDB";
 import { formatError } from "@/lib/constants/utils";
+import { convertToPlainObject, round2 } from "@/lib/utils";
+import { cartItemSchema, insertCartSchema } from "@/lib/validator";
 import { CartItem } from "@/types";
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
+
+
+const calcPrice = (items:CartItem[])=>{
+  const itemsPrice = round2(
+    items?.reduce((acc,item)=> acc + Number(item?.price) * item?.qty,0)
+  );
+  const shippingPrice = round2(itemsPrice > 100 ? 0 : 10);
+  const taxPrice = round2(0.15 * itemsPrice);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return {
+    itemsPrice:itemsPrice?.toFixed(2),
+    shippingPrice:shippingPrice?.toFixed(2),
+    taxPrice:taxPrice?.toFixed(2),
+    totalPrice:totalPrice?.toFixed(2)
+  }
+}
 
 
 export async function addItemToCart(data:CartItem){
@@ -15,12 +35,34 @@ export async function addItemToCart(data:CartItem){
     const session = await auth();
     const userId = session?.user?.id ? (session?.user?.id as string) : undefined;
     
+    const cart = await getMyCart();
 
-    return {
-      success:true,
-      message:'Item added to cart'
+    const item = cartItemSchema.parse(data);
+
+    const product = await prisma.product.findFirst({
+      where:{
+        id:item?.productId
+      }
+    })
+    if(!product){
+      throw new Error("product not found")
     }
-
+    if(!cart){
+      const newCart = insertCartSchema.parse({
+        userId:userId,
+        items:[item],
+        sessionCartId:sessionCartId,
+        ...calcPrice([item])
+      });
+      await prisma.cart.create({
+        data:newCart
+      });
+      revalidatePath(`/product/${product?.slug}`)
+      return {
+        success:true,
+        message:'Item added to cart'
+      }
+    } 
   }catch(error){
     console.log("cart",error);
     return {
@@ -29,3 +71,31 @@ export async function addItemToCart(data:CartItem){
     }
   }
 }
+
+export async function getMyCart(){
+  const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+    if(!sessionCartId){
+      throw new Error("Cart session not found");
+    }
+    const session = await auth();
+    const userId = session?.user?.id ? (session?.user?.id as string) : undefined;
+
+    const cart = await prisma.cart.findFirst({
+      where:userId ? {
+        userId:userId
+      }: {sessionCartId:sessionCartId}
+    })
+    if(!cart){
+      return undefined;
+    }
+    return convertToPlainObject({
+      ...cart,
+      items: cart?.items as CartItem[],
+      itemsPrice:cart?.itemsPrice?.toString(),
+      totalPrice:cart?.totalPrice?.toString(),
+      shippingPrice:cart?.shippingPrice?.toString(),
+      taxPrice:cart?.taxPrice?.toString(),
+
+    })
+
+} 
--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,14 @@ export function formatNumberWithDecimal(num:number):string{
   const [int,decimal] = num.toString().split('.');
   return decimal ? `${int}.${decimal.padEnd(2,'0')}` : `${int}.00`
 }
+
+
+export function round2(value:number | string){
+  if(typeof value === 'number'){
+    return Math.round((value + Number.EPSILON) * 100) / 100;
+  }else if(typeof value === 'string'){
+    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
+  }else{
+    throw new Error("Value is not a number or string");
+  }
+}
