cd0530ea8fd539ab8fbde150d81d91b14f2d8d19
mohamedeld <mohamed.azoz20010@gmail.com>
2025-02-01 22:33:35 +0200

    add deal count down component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import { getFeautredProducts, getLatestProducts } from "@/actions/product.action"
+import DealCountdown from "@/components/deal-countdown";
+import IconsBox from "@/components/icons-box";
 import ProductCarousel from "@/components/shared/products/product-carousel";
 import ProductList from "@/components/shared/products/product-list";
 import ViewAllProducts from "@/components/view-all-products";
@@ -15,6 +17,8 @@ const HomePage = async () => {
       {Array.isArray(featuredProducts) && featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={products || []} title="Newest Arrivals" limit={4}/>
       <ViewAllProducts/>
+      <DealCountdown/>
+      <IconsBox/>
     </div>
   )
 }
